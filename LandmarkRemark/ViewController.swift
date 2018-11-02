feea178a62d5cab3453d7a0a383122cf94a21c73
Haidar Mohammed <Haidarooz@haidars-macbook-pro.local>
2018-11-02 18:18:21 +1100

    Initial Commit

---
+++ LandmarkRemark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandmarkRemark
+//
+//  Created by Haidar Mohammed on 2/11/18.
+//  Copyright © 2018 Haidar AlOgaily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
